20f57812ecff0176b7713c4a4a5e16cfa88078b8
Debargha Bhattacharjee <debarghab.kvdima@gmail.com>
2017-04-09 23:11:53 +0530

    adding xml files (try 2)

--- app/src/main/java/com/example/root/trackr/MainActivity.java
+++ app/src/main/java/com/example/root/trackr/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.root.trackr;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
