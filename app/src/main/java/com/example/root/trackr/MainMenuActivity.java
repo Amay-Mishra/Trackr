dac2e701293b36fe4e2e4cded871bac1e50a8c5a
Debargha Bhattacharjee <debarghab.kvdima@gmail.com>
2017-07-11 01:26:29 +0530

    Designed MainMenuActivity.

--- app/src/main/java/com/example/root/trackr/MainMenuActivity.java
+++ app/src/main/java/com/example/root/trackr/MainMenuActivity.java
@@ -7,8 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -17,6 +21,8 @@ public class MainMenuActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private Toolbar mToolbar;
+    private TextView textView_tracking_status;
+    private Switch switch_enable_tracking;
 
 
     @Override
@@ -27,6 +33,8 @@ public class MainMenuActivity extends AppCompatActivity {
         initialize();
 
         addListenerForActionBar();
+
+        addListenerForSwitch();
     }
 
     public void initialize() {
@@ -34,6 +42,27 @@ public class MainMenuActivity extends AppCompatActivity {
         mToolbar= (Toolbar) findViewById(R.id.nav_action);
         mDrawerLayout= (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle= new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        textView_tracking_status = (TextView) findViewById(R.id.textViewTrackingStatus);
+        switch_enable_tracking = (Switch) findViewById(R.id.switchEnableTracking);
+    }
+
+    public void addListenerForSwitch() {
+        switch_enable_tracking.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        String trackingStatus;
+                        if (isChecked) {
+                            trackingStatus = getResources().getString(R.string.tracking_enabled);
+
+                        }
+                        else{
+                            trackingStatus = getResources().getString(R.string.tracking_disabled);
+                        }
+                        textView_tracking_status.setText(trackingStatus);
+                    }
+                }
+        );
     }
 
     public void addListenerForActionBar() {
